a32ff8ecba24c2dddc1c8a2c56cb67739af1d367
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2022-08-28 19:58:46 -0300

    Aulas.FundamentosJs

---
+++ funcao2.js
@@ -0,0 +1,18 @@
+const imprimirSoma = function (a, b){
+    console.log (a+b)
+}
+
+imprimirSoma(2,3)
+
+const soma = (a,b) =>{ //ARROW FUNCTION
+    return a + b
+}
+
+console.log(soma (5,7))
+
+const sub = (a,b) => a - b
+console.log (sub(10,2))
+
+const imp = a => console.log(a)
+imp("olha o tanto que é prático!!!")
+
---
+++ varLet.js
@@ -0,0 +1,18 @@
+// var numero = 1
+
+// {//bloco não é função
+//     var numero = 2 //imprime esse último valor
+//     console.log('dentro do bloco: ', numero) 
+// }
+
+// console.log('fora do bloco: ', numero)
+
+let num = 1
+    {
+        let num = 2
+        console.log('dentro do bloco: ', num)
+    }
+
+    console.log('fora do bloco: ', num)
+
+    
