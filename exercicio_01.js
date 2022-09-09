d408b9d34ee0cf6972a4342c88c6715673b63813
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2022-09-08 22:30:05 -0300

    exercicios

---
+++ exercicio_01.js
@@ -0,0 +1,11 @@
+/*01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma,
+subtração, multiplicação e divisão desses valores.*/
+
+
+
+
+function calculadora (operador1, operador2){
+    console.log(`soma = ${operador1+operador2}, subtração = ${operador1-operador2}, nultiplicação  = ${operador1*operador2}, divisão = ${operador1/operador2}`)
+}
+
+calculadora(2,1)
---
+++ exercicio_02.js
@@ -0,0 +1,21 @@
+/*02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+Equilátero: Os três lados são iguais.
+Isósceles: Dois lados iguais.
+Escaleno: Todos os lados são diferentes.
+Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua
+classificação quanto ao tamanho de seus lados.
+(Neste exemplo deve-se abstrair as condições matemáticas de existência de um triângulo) */
+
+function classificarTriangulo (lado1, lado2, lado3){
+    if(lado1 == lado2 && lado2 ==lado3){
+        return 'Equilatero'
+    } else if (lado1 == lado2 || lado2 == lado3 || lado1== lado3 ){
+        return 'isosceles'
+    }else{
+        return 'escaleno'
+    }
+}
+
+    console.log(classificarTriangulo(2,2,2))
+    console.log(classificarTriangulo(2,3,3))
+    console.log(classificarTriangulo(2,3,4))
